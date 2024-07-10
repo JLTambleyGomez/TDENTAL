f344274ee35f232fb533e1340582319fe6cd384a
koke <jorgetambleygomez@gmail.com>
2024-07-10 15:18:48 -0400

    d2

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports:[CommonModule],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
   isMenuOpen = false;
+  isScreenSmall = window.innerWidth < 768;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.isScreenSmall = window.innerWidth < 768;
+    if (!this.isScreenSmall) {
+      this.isMenuOpen = true;  // Mantén el menú abierto en pantallas grandes
+    }
   }
 }
